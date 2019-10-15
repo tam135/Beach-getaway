8fa2fc3962dae076ee86e9a15aadef36b518052f
Tam Phan <tnphan0204@gmail.com>
2019-10-15 19:38:29 -0400

    added basic routes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from "./pages/SingleRoom";
+import Error from './pages/Error';
+
+import { Route, Switch} from 'react-router-dom';
+
+import Navbar from './components/Navbar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms" component={Rooms} />
+          <Route exact path="/single-room" component={SingleRoom} />
+          <Route component={Error} />
+        </Switch>
+      </>
+    );
+  }
+}
