a7342659d689e03d76a597639cbfff748db20a2a
Tam Phan <tnphan0204@gmail.com>
2019-10-17 02:41:57 -0400

    prop types

--- src/components/Room.js
+++ src/components/Room.js
@@ -23,6 +23,14 @@ export default function Room({room}) {
   )
 }
 
+Room.propTypes= {
+  room: PropTypes.shape({
+    name:PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired
+  })
+}
 
 
 
