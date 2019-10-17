59e417de4d396e56cdc1fe20977cf366bccd4396
Tam Phan <tnphan0204@gmail.com>
2019-10-17 02:32:26 -0400

    Featured rooms section with 3 rooms and hover link

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/rooms" component={Rooms} />
-          <Route exact path="/single-room" component={SingleRoom} />
+          <Route exact path="/:slug" component={SingleRoom} />
           <Route component={Error} />
         </Switch>
       </>
--- src/context.js
+++ src/context.js
@@ -11,11 +11,28 @@ class RoomProvider extends Component {
         loading: true
     }
 
-    componentDidUpdate() {
+    componentDidMount = () => {
         let rooms = this.formatData(items)
+        console.log(rooms)
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms:rooms,
+            loading:false
+        })
     }
 
-    
+    formatData = (items) => {
+        let tempItems = items.map(item => {
+            let id = item.sys.id 
+            let images = item.fields.images.map(image => image.fields.file.url)
+
+            let room = {...item.fields, images, id}
+            return room
+        })
+        return tempItems
+    }
     render() {
         return (
             <RoomContext.Provider value={{...this.state}}>
