7f7279223a90fa55a7c55396e96bbce0747f9391
Tam Phan <tnphan0204@gmail.com>
2019-10-17 02:44:05 -0400

    merged

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import {RoomContext} from '../context'
 import Loading from './Loading';
@@ -24,29 +23,3 @@ export default class FeaturedRooms extends Component {
         )
     }
 }
-=======
-import React, { Component } from "react";
-import Title from "./Title";
-import { RoomContext } from "../context";
-import Room from "./Room";
-import Loading from "./Loading.js";
-export default class FeaturedRooms extends Component {
-    static contextType = RoomContext;
-
-    render() {
-        let { loading, featuredRooms: rooms } = this.context;
-
-        rooms = rooms.map(room => {
-            return <Room key={room.id} room={room} />;
-        });
-        return (
-            <section className="featured-rooms">
-                <Title title="featured rooms" />
-                <div className="featured-rooms-center">
-                    {loading ? <Loading /> : rooms}
-                </div>
-            </section>
-        );
-    }
-}
->>>>>>> adacffcb273199c4434ff1de399a512a56a87738
