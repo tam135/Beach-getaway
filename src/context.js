784d2f67ffe6e378917a8749cd120b77ece8fb4c
Tam Phan <tnphan0204@gmail.com>
2019-10-17 23:55:39 -0400

    Finished filter options, added breakfast and pet filters

--- src/context.js
+++ src/context.js
@@ -56,7 +56,7 @@ class RoomProvider extends Component {
     
     handleChange = event => {
         const target = event.target
-        const value = event.type === 'checkbox' ?
+        const value = target.type === 'checkbox' ?
         target.checked:target.value
         const name = event.target.name
         this.setState({
@@ -84,6 +84,20 @@ class RoomProvider extends Component {
         //filter by price
         tempRooms = tempRooms.filter(room => room.price <= price)
         
+        //filter by size
+        tempRooms = tempRooms.filter(room => room.size > minSize && room.size < maxSize)
+
+
+        //filter by freakfast
+        if(breakfast) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true) 
+        }
+        
+
+        //filter by pets
+        if(pets) {
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
 
         //change state
         this.setState({
